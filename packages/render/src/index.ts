1a723722eb075cf9c964f271df7e7acf55be52f1
13enBi <2210576534@qq.com>
2022-05-19 00:31:31 +0800

    refactor: processor map

--- packages/render/src/index.ts
+++ packages/render/src/index.ts
@@ -1,45 +1,28 @@
-import { processLink } from './link';
-import { processText } from './text';
-import { processImage } from './image';
+import { HadesNode, NodeType, isDisplayNone, isElement } from '@hades/layout';
+import { processRawText, processText } from './text';
 import { processView } from './view';
-import {
-    HadesNode,
-    NodeType,
-    HadesTextElement,
-    isDisplayNone,
-    isElement,
-    HadesViewElement,
-    HadesLinkElement,
-    HadesImageElement
-} from '@hades/layout';
+import { processLink } from './link';
+import { processImage } from './image';
+
+const PROCESSOR_MAP = {
+    [NodeType.VIEW]: processView,
+    [NodeType.TEXT]: processText,
+    [NodeType.LINK]: processLink,
+    [NodeType.IMAGE]: processImage,
+    [NodeType.RAW_TEXT]: processRawText
+};
 
 export const render = async (node: HadesNode, isCalcLayout = false) => {
     if (!node.isDirty && !isCalcLayout) return node.content;
     if (isElement(node) && isDisplayNone(node)) return '';
 
     if (isCalcLayout) node.yoga.calculateLayout();
-
     node.context.layout = node.yoga.getComputedLayout();
 
-    switch (node.type) {
-        case NodeType.VIEW:
-            await processView(node as HadesViewElement);
-            break;
-
-        case NodeType.TEXT:
-            processText(node as HadesTextElement);
-            break;
-
-        case NodeType.LINK:
-            processLink(node as HadesLinkElement);
-            break;
-
-        case NodeType.IMAGE:
-            await processImage(node as HadesImageElement);
-            break;
-
-        default:
-    }
+    const processor = PROCESSOR_MAP[node.type];
+    if (!processor) throw new Error(`Unknown node type: ${node.type}`);
+    // @ts-ignore
+    await processor(node);
 
     node.isDirty = false;
     return node.content;
